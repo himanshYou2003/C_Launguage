184248ef8e6671cb0d1cfb90e0975c2199ad030b
himanshyou2003 <himanshukumar935437@gmail.com>
2023-01-27 12:45:35 +0530

    Testing

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+
+    printf("enter the number of a:\n");
+    scanf("%d",&a);
+    printf("enter the number of b \n");
+    scanf("%d",&b);
+    printf("The sum of %d and %d is = %d",a,b,a+b);
+    return 0;
+}
